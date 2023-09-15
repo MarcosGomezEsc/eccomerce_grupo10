f5472a03e3d242e12c9aec95e9876f3d5282f732
MarcosGomezEsc <142756989+MarcosGomezEsc@users.noreply.github.com>
2023-09-15 16:04:21 -0300

    quiero ver si de esta manera me sale el json

--- obtenerDataProd.js
+++ obtenerDataProd.js
@@ -1,24 +1,23 @@
-export const ObtenerDataProd = async()=>{
-    const res = await fetch('https://car-data.p.rapidapi.com/cars?limit=10&page=0')
-    if(!res.ok) throw new Error("respomse not ok")
-    const data = await res.json()
-    console.log(data)
-    return(data)
+import React from 'react';
+import { useDataContext } from '../cars.jsx'
+
+function MiComponente() {
+  const { data, loading } = useDataContext();
+
+  if (loading) {
+    return <div>Cargando datos...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Mis Datos</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.nombre}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-// const url = 'https://car-data.p.rapidapi.com/cars?limit=10&page=0';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '66da1b4532msh6c80d44983c663bp1eaf1fjsn5d23d65a10a5',
-// 		'X-RapidAPI-Host': 'car-data.p.rapidapi.com'
-// 	}
-// };
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
+export default MiComponente;
