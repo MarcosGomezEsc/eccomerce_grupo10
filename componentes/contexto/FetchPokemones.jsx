d13f578d1859abf866be7268fa0d4620fa892bf0
MarcosGomezEsc <142756989+MarcosGomezEsc@users.noreply.github.com>
2023-09-17 17:45:51 -0300

    Update FetchPokemones.jsx

--- componentes/contexto/FetchPokemones.jsx
+++ componentes/contexto/FetchPokemones.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, createContext } from 'react';
 
-// Reemplaza esta URL con tu enlace de API
-
+// Reemplaza esta URL con tu enlace de API para obtener detalles de Pokémon
 const API_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 export const PokemonContext = createContext();
@@ -11,19 +10,33 @@ export function PokemonProvider({ children }) {
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1); // Estado para la página actual
 
-  
-  
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${API_URL}?page=${currentPage}`);
+        const response = await fetch(`${API_URL}?offset=${(currentPage - 1) * 20}&limit=20`);
         if (!response.ok) {
           throw new Error('Error al obtener datos');
         }
         const newData = await response.json();
-        setData(newData);
         
-         
+        // Mapear los resultados para obtener detalles adicionales, incluida la URL de la imagen
+        const pokemonDataPromises = newData.results.map(async (pokemon) => {
+          const pokemonResponse = await fetch(pokemon.url);
+          if (!pokemonResponse.ok) {
+            throw new Error(`Error al obtener detalles de ${pokemon.name}`);
+          }
+          const pokemonDetails = await pokemonResponse.json();
+          return {
+            id: pokemonDetails.id,
+            name: pokemonDetails.name,
+            image: pokemonDetails.sprites.front_default, // URL de la imagen del Pokémon
+          };
+        });
+
+        // Esperar a que se completen todas las solicitudes para obtener detalles
+        const pokemonDetailsData = await Promise.all(pokemonDataPromises);
+
+        setData(pokemonDetailsData);
       } catch (error) {
         console.error('Error con Fetch:', error);
       } finally {
@@ -32,8 +45,6 @@ export function PokemonProvider({ children }) {
     };
     fetchData();
   }, [currentPage]);
-
-
 
   const contextValue = { data, loading, setCurrentPage }; // Agregar setCurrentPage al contexto
 
