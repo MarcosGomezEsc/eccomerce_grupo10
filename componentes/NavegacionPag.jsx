94631ce4888e87c168c46cfc1dd0eef70c775b80
MarcosGomezEsc <142756989+MarcosGomezEsc@users.noreply.github.com>
2023-09-19 19:11:40 -0300

    Update NavegacionPag.jsx

--- componentes/NavegacionPag.jsx
+++ componentes/NavegacionPag.jsx
@@ -5,19 +5,37 @@ import PokeLista from "./PokeLista"
 export const NavegacionPag =()=>{
     return (
 
-            
-            <header className="header">
-                <div className="logo">
-                    <img src="https://www.pngall.com/wp-content/uploads/4/Pokemon-Pokeball-PNG-Clipart.png" alt="" />
-                </div>
-                
-                <nav className="nav">
-                    <ul className="ul">
-                        <li>Log In</li>
-                    </ul>
-                </nav>
-            <Outlet></Outlet>
-            </header>
+        <>
+        <header className="header">
+          <div className="logo">
+            <img
+              src="https://www.pngall.com/wp-content/uploads/4/Pokemon-Pokeball-PNG-Clipart.png"
+              alt=""
+            />
+          </div>
+  
+          <nav className="nav">
+            <ul className="ul">
+              <li>
+                <a id="/">Inicio</a>
+              </li>
+              <li>
+                <a id="/productos">Productos</a>
+              </li>
+              <li>
+                <a id="/acerca-de">Acerca de</a>
+              </li>
+              <li>
+                <a id="/contacto">Contacto</a>
+              </li>
+            </ul>
+          </nav>
+          <Outlet />
+        </header>
+        <h2>Le damos la bienvenida a PokeTienda</h2>
+        <h3>¿Qué personaje desea llevar?</h3>
+        
+      </>
 
 
     )
