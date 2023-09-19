ef8a15b0edf2a3c7f7258fde1a6206f4439efd26
MarcosGomezEsc <142756989+MarcosGomezEsc@users.noreply.github.com>
2023-09-19 19:12:28 -0300

    Add files via upload

---
+++ componentes/AcercaDe.jsx
@@ -0,0 +1,43 @@
+export default function AcercaDe(){
+    return (
+    <>
+    <h2>¿Quienes Somos?</h2>
+    <section>
+        <div className="imgSection">
+            <img src="https://via.placeholder.com/250x250" alt="" />
+        </div>
+        <div className="sectionData">
+            <h2>Nosotros somos PokeTienda</h2>
+            <h4>¿Qué hacemos?</h4>
+            <p>Bueno, somos una empresa encargada de ecologizar todos los materiales plásticos</p>
+        </div>
+        <div className="imgSection2">
+            <img src="https://via.placeholder.com/250x250" alt="" />
+        </div>
+        <div className="sectionData3">
+            <h2>Seguramente te preguntes Cómo es que nosotros estamos taaaan descriptivos en esto</h2>
+            <h4>Dejanos sorprenderte</h4>
+            <p>Nosotros tomamos material reciclado, rebuscamos entre los materiables desechables todas aquellas cosas que se ven muy tentadoramente manualizable (¿Existe esa palabra?) y nos encargamos de que llegue a sus manos en forma de pokeamigo
+            </p>
+        </div>
+        <div className="sectionData3">
+            <h2>¿Acaso no es genial?</h2>
+            <h4>¿Que cómo lo hacemos?</h4>
+            <p>Muy fácil. Agarramos botellas de plástico. Nooo se preocupe, están en lavandina un bueen tiempo,
+                después, le agregamos una pizca de, bueno, creatividad.
+                Pasado su tiempo, le agregamos por esa corteplástico que nos devuelve tiritas de plástico, tenemos una booocha de material
+            </p>
+        <div className="imgSection2">
+            <img src="https://via.placeholder.com/250x250" alt="" />
+        </div>
+        <p>Pasado Luego por el calentador de materiales, logramos conseguir
+            obtener el filamento buscado para que este sea de rápido acceso
+            a nuestra impresora 3d SI SI, hecho por nosotros mismos!!!
+            !!!
+        </p>
+        </div>
+        
+    </section>
+    </>
+    )
+}
