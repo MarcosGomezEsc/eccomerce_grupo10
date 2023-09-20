f5bc62e360d8bf74324e3d423cf0c81046c6fdd7
MarcosGomezEsc <142756989+MarcosGomezEsc@users.noreply.github.com>
2023-09-20 09:42:53 -0300

    Update obtenerDataPokemones.jsx

--- componentes/obtenerDataPokemones.jsx
+++ componentes/obtenerDataPokemones.jsx
@@ -1,8 +1,7 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useCallback } from "react";
 import { PokemonContext } from "../contexto/FetchPokemones";
 import PokemonCards from "./PokemonCards";
 import '../cssComponentes/PokeCards.css'
-
 
 
 function DataPokemo() {
@@ -10,12 +9,14 @@ function DataPokemo() {
   const [searchTerm, setSearchTerm] = useState(""); // Estado para almacenar el término de búsqueda
   const [lista, setlista] = useState("name"); // Estado para el tipo de orden (por nombre o por ID)
 
+
+
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
   const handleSort = (event) => {
-    setlista(event.target.value); // Actualizar el tipo de orden cuando el usuario elija una opción de orden
+    setlista(event.target.value); // Actualizo el tipo de orden cuando el usuario elija una opción de orden
   };
 
   // Función para ordenar los datos
@@ -27,15 +28,50 @@ function DataPokemo() {
     }
     return 0;
   });
+    //boton comprar
 
-  return (
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const [carrito, setCarrito] = useState([]); // Inicialmente el carrito está vacío
+    const [precioTotal, setPrecioTotal] = useState(0);
+
+
+    const openPopup = useCallback((product) => {
+      setSelectedProduct(product);
+      setIsPopupOpen(true);
+    }, [setSelectedProduct, setIsPopupOpen]);
+    
+      
+    const closePopup = useCallback(() => {
+        setIsPopupOpen(false);
+    }, []);
+
+    const addToCart = () => {
+      if (selectedProduct) {
+        const productoAgregado = {
+          name: selectedProduct.name,
+          id: selectedProduct.id,
+        };
+    
+        // Agrego el nuevo producto al carrito
+        setCarrito((prevCarrito) => [...prevCarrito, productoAgregado]);
+        closePopup();
+    
+        // Calculo el precio total sumando los ids(x265 para el precio) en el carrito actualizado
+        const nuevoPrecioTotal = carrito.reduce((total, producto) => total + (producto.id*265), (selectedProduct.id*265));
+        setPrecioTotal(nuevoPrecioTotal);
+      }
+    };
+    
+
+    return (
     <div>
       <input
         type="text"
         placeholder="Buscar personaje"
         value={searchTerm}
         onChange={handleSearch}
-      />
+        />
       
       <div>
         <select className="select" value={lista} onChange={handleSort}>
@@ -44,33 +80,58 @@ function DataPokemo() {
         </select>
       </div>
 
+        
+        {isPopupOpen &&  sortedData &&  (
+          <div className="popup">
+            <img src={sortedData.image} alt={sortedData.name} />
+            <p>Precio {parseInt(selectedProduct.id, 10) * 265}</p>
+        <p>¿Desea comprar este producto?</p>
+        <button onClick={() => closePopup()}>Cancelar</button>
+        <button onClick={() => addToCart()}>Confirmar</button>
+          </div>
+        )}
+
+
       {loading ? (
         <p>Cargando...</p>
-      ) : (
-        <section>
+        ) : (
+          <section>
           {data.length > 0 ? (
-        data && sortedData
-          .filter((item) => {
-            const searchTermLower = searchTerm.toLowerCase();
-
-          return (
-            item.name.toLowerCase().includes(searchTermLower) ||
-            (item.id*265).toString().includes(searchTermLower)
-          );
-        })
-
-    .map((item, index) => (
-      <article key={index}>
+            data && sortedData
+            .filter((item) => {
+              const searchTermLower = searchTerm.toLowerCase();
+                return (
+                  item.name.toLowerCase().includes(searchTermLower) ||
+                  (item.id*265).toString().includes(searchTermLower)
+                );
+              })
+              
+              .map((item, index) => (
+                <article key={index}>
         <PokemonCards item={item}></PokemonCards>
+        <button onClick={() => openPopup(item)}>Comprar</button>
       </article>
     ))
-) : (
-  <p>No se encontraron resultados.</p>
-)}
+    ) : (
+      <p>No se encontraron resultados.</p>
+      )}
         </section>
       )}
+      
+      <div className="carrito">
+    <h2>Carrito de Compras</h2>
+    <ul>
+      {carrito.map((producto, index) => (
+        <li key={index}>
+          {producto.name}_____________${(producto.id)*265}
+        </li>
+      ))}
+    </ul>
+    <p>Total: {precioTotal}</p>
+  </div>
     </div>
   );
 }
 
 export default DataPokemo;
+
